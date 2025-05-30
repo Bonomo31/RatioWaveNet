cfd6719c0481692f78b5728c1a966490500caa4d
Bonomo31 <gius31bon@gmail.com>
2025-05-30 19:30:33 +0200

    New test

--- HGD/models.py
+++ HGD/models.py
@@ -39,7 +39,198 @@ from tensorflow.keras import backend as K
 
 from attention_models import attention_block
 
-
+#import models
+#
+#def getModel(model_name, dataset_conf, from_logits = False):
+#
+#    n_classes = dataset_conf.get('n_classes')
+#    n_channels = dataset_conf.get('n_channels')
+#    in_samples = dataset_conf.get('in_samples')
+#
+#    # Select the model
+#    if(model_name == 'RockNet'):
+#        # Train using the proposed ATCNet model: https://ieeexplore.ieee.org/document/9852687
+#        model = models.RockNet_(
+#            # Dataset parameters
+#            n_classes = n_classes,
+#            in_chans = n_channels,
+#            in_samples = in_samples,
+#            # Sliding window (SW) parameter
+#            n_windows = 5,
+#            # Attention (AT) block parameter
+#            attention = 'mha', # Options: None, 'mha','mhla', 'cbam', 'se'
+#            # Convolutional (CV) block parameters
+#            eegn_F1 = 16,
+#            eegn_D = 2,
+#            eegn_kernelSize = 64,
+#            eegn_poolSize = 7,
+#            eegn_dropout = 0.5,
+#            # Temporal convolutional (TC) block parameters
+#            tcn_depth = 2,
+#            tcn_kernelSize = 4,
+#            tcn_filters = 64,
+#            tcn_dropout = 0.3,
+#            tcn_activation='elu',
+#            )
+#    elif(model_name == 'RatioWaveNet'):
+#        model = models.RatioWaveNet(
+#            n_classes = n_classes,
+#            in_chans = n_channels,
+#            in_samples = in_samples,
+#            n_windows = 5,
+#            attention = 'mha',
+#            eegn_F1 = 16,
+#            eegn_D = 2,
+#            eegn_kernelSize = 64,
+#            eegn_poolSize = 7,
+#            eegn_dropout = 0.3,
+#            tcn_depth = 2,
+#            tcn_kernelSize = 4,
+#            tcn_filters = 32,
+#            tcn_dropout = 0.3,
+#            tcn_activation='elu',
+#        )
+#    elif(model_name == 'RockNetB'):
+#        model = models.RockNetB(
+#            n_classes = n_classes,
+#            in_chans = n_channels,
+#            in_samples = in_samples,
+#            n_windows = 5,
+#            attention = 'mha',
+#            eegn_F1 = 16,
+#            eegn_D = 2,
+#            eegn_kernelSize = 64,
+#            eegn_poolSize = 7,
+#            eegn_dropout = 0.5,
+#            tcn_depth = 2,
+#            tcn_kernelSize = 4,
+#            tcn_filters = 32,
+#            tcn_dropout = 0.3,
+#            tcn_activation='elu',
+#        )
+#    elif(model_name == 'RockNetC'):
+#        model = models.RockNetC(
+#            n_classes = n_classes,
+#            in_chans = n_channels,
+#            in_samples = in_samples,
+#            n_windows = 5,
+#            attention = 'mha',
+#            eegn_F1 = 16,
+#            eegn_D = 2,
+#            eegn_kernelSize = 64,
+#            eegn_poolSize = 7,
+#            eegn_dropout = 0.5,
+#            tcn_depth = 2,
+#            tcn_kernelSize = 4,
+#            tcn_filters = 32,
+#            tcn_dropout = 0.3,
+#            tcn_activation='elu',
+#        )
+#    elif(model_name == 'RockNetX'):
+#        model = models.RockNetX(
+#            n_classes = n_classes,
+#            in_chans = n_channels,
+#            in_samples = in_samples,
+#            n_windows = 5,
+#            attention = 'transformer',
+#            eegn_F1 = 16,
+#            eegn_D = 2,
+#            eegn_kernelSize = 64,
+#            eegn_poolSize = 7,
+#            eegn_dropout = 0.5,
+#            tcn_depth = 3,
+#            tcn_kernelSize = 4,
+#            tcn_filters = 64,
+#            tcn_dropout = 0.3,
+#            tcn_activation='gelu',
+#        )
+#    elif(model_name == 'RockNetY'):
+#        model = models.RockNetY(
+#            n_classes = n_classes,
+#            in_chans = n_channels,
+#            in_samples = in_samples,
+#            n_windows = 5,
+#            attention = 'mha',
+#            eegn_F1 = 16,
+#            eegn_D = 2,
+#            eegn_kernelSize = 64,
+#            eegn_poolSize = 7,
+#            eegn_dropout = 0.5,
+#            tcn_depth = 2,
+#            tcn_kernelSize = 4,
+#            tcn_filters = 32,
+#            tcn_dropout = 0.3,
+#            tcn_activation='elu',
+#        )
+#    elif(model_name == 'RockNetZ'):
+#        model = models.RockNetZ(
+#            n_classes = n_classes,
+#            in_chans = n_channels,
+#            in_samples = in_samples,
+#            n_windows = 5,
+#            attention = 'mha',
+#            eegn_F1 = 16,
+#            eegn_D = 2,
+#            eegn_kernelSize = 64,
+#            eegn_poolSize = 7,
+#            eegn_dropout = 0.5,
+#            tcn_depth = 2,
+#            tcn_kernelSize = 4,
+#            tcn_filters = 32,
+#            tcn_dropout = 0.3,
+#            tcn_activation='relu',
+#        )
+#    elif(model_name == 'ATCNet'):
+#        # Train using the proposed ATCNet model: https://ieeexplore.ieee.org/document/9852687
+#        model = models.ATCNet_(
+#            # Dataset parameters
+#            n_classes = n_classes,
+#            in_chans = n_channels,
+#            in_samples = in_samples,
+#            # Sliding window (SW) parameter
+#            n_windows = 5,
+#            # Attention (AT) block parameter
+#            attention = 'mha', # Options: None, 'mha','mhla', 'cbam', 'se'
+#            # Convolutional (CV) block parameters
+#            eegn_F1 = 16,
+#            eegn_D = 2,
+#            eegn_kernelSize = 64,
+#            eegn_poolSize = 7,
+#            eegn_dropout = 0.3,
+#            # Temporal convolutional (TC) block parameters
+#            tcn_depth = 2,
+#            tcn_kernelSize = 4,
+#            tcn_filters = 32,
+#            tcn_dropout = 0.3,
+#            tcn_activation='elu',
+#            )
+#    elif(model_name == 'TCNet_Fusion'):
+#        # Train using TCNet_Fusion: https://doi.org/10.1016/j.bspc.2021.102826
+#        model = models.TCNet_Fusion(n_classes = n_classes, Chans=n_channels, Samples=in_samples)
+#    elif(model_name == 'EEGTCNet'):
+#        # Train using EEGTCNet: https://arxiv.org/abs/2006.00622
+#        model = models.EEGTCNet(n_classes = n_classes, Chans=n_channels, Samples=in_samples)
+#    elif(model_name == 'EEGNet'):
+#        # Train using EEGNet: https://arxiv.org/abs/1611.08024
+#        model = models.EEGNet_classifier(n_classes = n_classes, Chans=n_channels, Samples=in_samples)
+#    elif(model_name == 'EEGNeX'):
+#        # Train using EEGNeX: https://arxiv.org/abs/2207.12369
+#        model = models.EEGNeX_8_32(n_timesteps = in_samples , n_features = n_channels, n_outputs = n_classes)
+#    elif(model_name == 'DeepConvNet'):
+#        # Train using DeepConvNet: https://doi.org/10.1002/hbm.23730
+#        model = models.DeepConvNet(nb_classes = n_classes , Chans = n_channels, Samples = in_samples)
+#    elif(model_name == 'ShallowConvNet'):
+#        # Train using ShallowConvNet: https://doi.org/10.1002/hbm.23730
+#        model = models.ShallowConvNet(nb_classes = n_classes , Chans = n_channels, Samples = in_samples)
+#    elif(model_name == 'MBEEG_SENet'):
+#        # Train using MBEEG_SENet: https://www.mdpi.com/2075-4418/12/4/995
+#        model = models.MBEEG_SENet(nb_classes = n_classes , Chans = n_channels, Samples = in_samples)
+#
+#    else:
+#        raise Exception("'{}' model is not supported yet!".format(model_name))
+#
+#    return model
+#
 #%% The novel proposed model
 def RockNet_(n_classes, in_chans = 22, in_samples = 1125, n_windows = 5, attention = 'mha',
            eegn_F1 = 16, eegn_D = 2, eegn_kernelSize = 64, eegn_poolSize = 7, eegn_dropout=0.5,
